71c5979b73e12ddf50cf7c16a024e0a9ec2c7436
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-10-01 18:22:25 +0200

    trim string from both sides

--- src/lib/validators/store.ts
+++ src/lib/validators/store.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const storeValidator = z.object({
-  name: z.string().min(1),
+  name: z.string().trim().min(1),
 })
 
 export type StorePayload = z.infer<typeof storeValidator>
